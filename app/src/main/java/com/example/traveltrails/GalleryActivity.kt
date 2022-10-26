e313ddc12a49a6e93db3af77632eab865501f245
rhudson11 <ryanhudson@Ryans-MacBook-Pro.local>
2022-10-25 20:00:40 -0400

    3d model example

--- app/src/main/java/com/example/traveltrails/GalleryActivity.kt
+++ app/src/main/java/com/example/traveltrails/GalleryActivity.kt
@@ -1,24 +1,34 @@
 package com.example.traveltrails
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class GalleryActivity : AppCompatActivity() {
 
     private lateinit var start_trail_button: Button
+    private lateinit var view3Dbutton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
 
         start_trail_button = findViewById(R.id.start_trail_button)
+        view3Dbutton = findViewById(R.id.view3Dexample)
 
         start_trail_button.setOnClickListener { v: View ->
             val intent = Intent(this, StartTrailActivity::class.java)
             startActivity(intent)
+        }
+
+        view3Dbutton.setOnClickListener { v: View ->
+            val sceneViewerIntent = Intent(Intent.ACTION_VIEW)
+            sceneViewerIntent.data = Uri.parse("https://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf")
+            sceneViewerIntent.setPackage("com.google.android.googlequicksearchbox")
+            startActivity(sceneViewerIntent)
         }
     }
 }
