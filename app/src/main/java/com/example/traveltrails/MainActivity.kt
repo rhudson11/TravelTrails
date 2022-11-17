ac6fcb905b192dc4d1919e032502ab04d0feef51
rhudson11 <ryanhudson@Ryans-MacBook-Pro.local>
2022-11-16 21:51:26 -0500

    new look for home screen

--- app/src/main/java/com/example/traveltrails/MainActivity.kt
+++ app/src/main/java/com/example/traveltrails/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.traveltrails
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var login_pass_field: EditText
     private lateinit var login_button: Button
     private lateinit var register_button: Button
+    private lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,15 +28,10 @@ class MainActivity : AppCompatActivity() {
         login_button = findViewById(R.id.login_button)
         register_button = findViewById(R.id.register_button)
 
+        preferences = getSharedPreferences("traveltrails", Context.MODE_PRIVATE)
+
         login_button.setOnClickListener { v: View ->
-            doAsync {
-                val s: String = ServerManager().test_server();
-                runOnUiThread {
-                    val toast =
-                            Toast.makeText(this@MainActivity, s, Toast.LENGTH_LONG)
-                    toast.show()
-                }
-            }
+            preferences.edit().putString("username", login_user_field.text.toString()).apply()
 
             val intent = Intent(this, MapsActivity::class.java)
             startActivity(intent)
